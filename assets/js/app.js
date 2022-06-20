b129c1e5b8760e2373cce38492f6c03651739e2f
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2022-06-19 21:36:38 -0300

    Some JS updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,28 @@
 let gameArea = $('#game-area');
 let playerMessageInput = $('#input-action');
 let backpack = $('#backpack');
+let body = $('#body');
+
+let unknowColor = '#e35a0b';
+let npcColor = '#1ba112';
+let storeColor = '#dfe30b';
+let systemColor = '#ff00fb';
+let delay = 1000;
+
+let playerName;
 
 $(document).ready(()=>{   
     
     $("#input-form").submit(function(e){        
         return false;
     });
+
+    playerName = 'Player';
+
+    game();
 })
 
-function geraMensagem(nome, mensagem, tipo){
+function geraMensagem(nome, color_name, mensagem, tipo){
 
     if (mensagem == '' || mensagem == null ){
         return false
@@ -24,6 +37,7 @@ function geraMensagem(nome, mensagem, tipo){
     message.setAttribute('class', tipo)
 
     messageTitle.appendChild(document.createTextNode(nome));
+    messageTitle.style.color = color_name;
     message.appendChild(document.createTextNode(mensagem));
 
     divMensagem.appendChild(messageTitle);
@@ -32,8 +46,6 @@ function geraMensagem(nome, mensagem, tipo){
     $('#game-area').append(divMensagem);
 
     gameArea.scrollTop(gameArea.prop('scrollHeight'));
-
-    console.log(gameArea.prop('scrollHeight'))
 
 }
 
@@ -52,4 +64,71 @@ function openBackpack(){
 function closeBackpack(){
     backpack.removeClass('bp-open');
     backpack.addClass('bp-close');
+}
+
+function setPlayerName(){
+    playerName = $('#text-input').val();
+    closePopUp('#text-input-pop-up');
+}
+
+function getPlayerName(){
+    return playerName;
+}
+
+function closePopUp(id){
+    let popUp = $(id);
+    popUp.addClass('fade-out-bck');
+
+    setTimeout(() => {
+       popUp.remove(); 
+    }, 5000);
+}
+
+function geraTextPopUp(descricao, funcao, atributos){
+
+    let sectionPopUp = document.createElement('section');
+    let divInnerPopUp = document.createElement('div');
+    let message = document.createElement('p');
+    let popUpInput = document.createElement('input');
+    let divButtonArea = document.createElement('div');
+    let confirmButton = document.createElement('button');
+
+    sectionPopUp.setAttribute('class', 'text-input-pop-up');
+    sectionPopUp.setAttribute('id', 'text-input-pop-up');
+    popUpInput.setAttribute('id', 'text-input');
+    popUpInput.setAttribute('type', 'text');
+    popUpInput.setAttribute('name', 'text-input');
+    popUpInput.setAttribute('autoComplete', 'none');
+    divInnerPopUp.setAttribute('class', 'input-pop-up');
+    divButtonArea.setAttribute('class', 'popup-button-area');
+    confirmButton.setAttribute('id', 'confirm-button');
+    confirmButton.setAttribute('onclick', funcao + '(' + atributos +')');
+
+    confirmButton.appendChild(document.createTextNode('Confirmar'));
+    message.appendChild(document.createTextNode(descricao));
+
+    divButtonArea.appendChild(confirmButton);
+
+    divInnerPopUp.appendChild(message);
+    divInnerPopUp.appendChild(popUpInput);
+    divInnerPopUp.appendChild(divButtonArea);
+
+    sectionPopUp.appendChild(divInnerPopUp);
+
+    body.append(sectionPopUp);
+}
+
+// game begin
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function game(){
+    sleep(delay).then(() => {geraMensagem('Voz Desconhecida', unknowColor, 'Acorde, você precisa escapar!', 'warning-message')});
+    sleep(delay*1.5).then(() => {geraMensagem('Narrador', systemColor, 'Você acorda em uma cela de prisão velha e totalmente suja, sem saber por que está ali.', 'system-message')});
+    sleep(delay*3).then(() => {geraMensagem('Voz Desconhecida', unknowColor, 'Pode me dizer seu nome?', 'normal-message')});
+    sleep(delay*3.5).then(() => geraTextPopUp('Diga seu nome à voz desconhecida', 'setPlayerName', ''));
+    
 }
